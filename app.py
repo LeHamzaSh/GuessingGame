238658d99176ff8d794ef3c0f5cb62f8800faf71
LeHamzaSh <hrsheikh05@gmail.com>
2021-07-05 20:12:38 +0400

    Final Commit:
    1.Added if conditions
    2.Tested code
    3.Completed Program

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ guess_limit = 3
 while guess_count < guess_limit:
     guess = int(input('Guess: '))
     guess_count += 1
+
+    if guess == secret_number:
+        print("Yaaay! You Won!!")
+        break
+
+else:
+    print('Sorry you failed!!')
